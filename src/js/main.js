689b7a1db374949682428a4c5409546ba907819d
Krog-13 <virtualspook@gmail.com>
2024-04-08 00:11:42 +0500

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,8 @@
 
 let tg = window.Telegram.WebApp;
-let buy = document.getElementById("buy")
 let order = document.getElementById("order")
 tg.expand()
 
-buy.addEventListener("click", () => {
-    document.getElementById("main").style.display = "none"
-    document.getElementById("form").style.display = "block"
-    document.getElementById("address").value = "Leepstick"
-    document.getElementById("myname").value = "Test"
-});
 
 let initdata = tg.initData
 let dataunsave = tg.initDataUnsafe
@@ -17,15 +10,9 @@ let dataunsave = tg.initDataUnsafe
 order.addEventListener("click", () => {
 
     console.log("Init data above")
-    console.log(initdata)
-    console.log(dataunsave)
-    console.log(dataunsave.user)
-    let address = document.getElementById("address").value
     let price = document.getElementById("price").value
     let code = document.getElementById("code").value
-    console.log(address)
     let data = {
-        address: address,
         price: price,
         code: code
     }
